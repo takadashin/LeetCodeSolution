4bf237c3997a99686404a2bffe302fe7dfbf71c7
takadashin <khaivo17@yahoo.com>
2023-11-26 14:19:03 -0500

    Create Solution2.cpp

---
+++ 645. Set Mismatch/Solution2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+
+        
+         for(int i = 0;i < nums.size();i++)
+         {
+            while(nums[i] != nums[nums[i]-1])
+            swap(nums[i],nums[nums[i]-1]);
+         }
+
+        // sort(nums.begin(),nums.end());
+
+        for(int i = 0;i < nums.size();i++)
+        {
+
+            if(nums[i] != i+1) return {nums[i],i+1};
+
+        }
+
+        return {0,0};
+
+    }
+};
