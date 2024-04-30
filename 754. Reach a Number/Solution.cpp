96843493a769168eb807dac96cac680d838db894
Khai Vo <khaivo17@yahoo.com>
2024-04-30 10:32:19 -0400

    Create Solution.cpp

---
+++ 754. Reach a Number/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        int step = 0, sum = 0;
+        while(sum < target)
+        {step++;sum+= step;}
+
+        while((sum - target)% 2 != 0 ) {step++;sum+= step;}
+
+        return step;
+    }
+};
